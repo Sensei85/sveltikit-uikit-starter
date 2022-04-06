d8a3b2808978e921b6c5f6af671f2b5e43c64a3e
sensei85 <agbottahw@gmail.com>
2022-04-06 02:16:49 -0700

    [UPDATE] - Updated to render nav of documentation page in responsive mode when you are actually in docs page. Changed off canvas mode to push

--- src/lib/components/partials/MobileSideMenu.svelte
+++ src/lib/components/partials/MobileSideMenu.svelte
@@ -1,20 +1,22 @@
 <script>
+  import { page } from "$app/stores"
+  import { componentsDocsLinks } from '$lib/stores/site/Util';
   // @ts-ignore
   import { navLeftLinks, navRightLinks } from '$stores/site/Util.js'
 </script>
 
-<div id="side-nav" uk-offcanvas="overlay: true">
+<div id="side-nav" uk-offcanvas="overlay: true; mode: push">
   <div class="uk-offcanvas-bar">
-    <button class="uk-offcanvas-close" type="button" uk-close></button>
-    <h3>Online Portal</h3>
-    
     <div class="uk-width-1-1">
-      <ul class="uk-nav-default uk-list-large uk-nav-parent-icon" uk-nav>
+      <ul class="uk-nav-default uk-nav-parent-icon" uk-nav>
+        <li class="uk-nav-header">GENERAL</li>
+        <li class="uk-nav-divider"></li>
+
         {#each $navLeftLinks as link}
           {#if link?.hasSubMenu === true}
             <li class="uk-parent">
-              <a class="uk-text-bold" href="{link?.path}">{link?.title}</a>
-              <ul class="uk-nav-sub uk-list-large">
+              <a href="{link?.path}">{link?.title}</a>
+              <ul class="uk-nav-sub">
                 {#each link?.subLinks as link}
                   <li>
                     <a 
@@ -28,16 +30,33 @@
               </ul>
             </li>
           {:else}
-            <li class="uk-text-bold"><a href="{link?.path}">{link?.title}</a></li>
+            <li><a href="{link?.path}">{link?.title}</a></li>
           {/if}
         {/each}
         <li class="uk-nav-divider uk-margin-top"></li>
         {#each $navRightLinks as link}
           <li>
-            <a class="uk-text-bold" href="{link?.path}">{link?.title}</a>
+            <a href="{link?.path}">{link?.title}</a>
           </li>
         {/each}
       </ul>
+
+      {#if $componentsDocsLinks && $page.url.pathname.includes("docs") } 
+      <!-- Check if "docs" is in the path name of our url and renders the nav for documentation page in the mobile off canvas menu -->
+        <ul class="uk-nav uk-nav-default uk-margin-top">
+          <li class="uk-nav-header">COMPONENTS</li>
+          <li class="uk-nav-divider"></li>
+          {#each $componentsDocsLinks as item}
+            <li 
+              class:uk-active={item?.path === $page.url.pathname}>
+              <a 
+                href="{item?.path}">
+                {item?.title}
+              </a>
+            </li>
+          {/each}
+        </ul>
+      {/if}
     </div>
   </div>
 </div>
