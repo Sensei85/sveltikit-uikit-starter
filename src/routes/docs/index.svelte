4709778ac4d380d79ce80190f8a5e4c9aea3fb50
sensei85 <agbottahw@gmail.com>
2022-04-06 03:22:26 -0700

    [NEW] - Main page for documentation route

---
+++ src/routes/docs/index.svelte
@@ -0,0 +1,31 @@
+<svelte:head>
+  <title>Components</title>
+</svelte:head>
+
+<script>
+  import MiniFooter from "$components/bites/MiniFooter.svelte";
+  import Breadcrumbs from "$components/bites/Breadcrumbs.svelte";
+
+  export let crumbs = [
+    {
+      title: 'Home',
+      path: '/',
+      icon: `` // could be an svg or a class based icon element,
+    },
+    {
+      title: 'Docs',
+      path: '/docs',
+      icon: `` // could be an svg or a class based icon element,
+    },
+  ]
+</script>
+
+<main uk-height-viewport="expand: true">
+  <Breadcrumbs {crumbs} />
+
+  <div class="uk-container uk-margin-top">
+    <h4 class="uk-heading uk-heading-bullet">INTRODUCTION</h4>
+  </div>
+</main>
+
+<MiniFooter />
