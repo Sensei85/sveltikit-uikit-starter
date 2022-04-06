2574e40eb292c343dbe3cae0a62e0297968d5023
sensei85 <agbottahw@gmail.com>
2022-04-06 03:20:59 -0700

    [NEW] - Child layout component in docs/components dir that inherits the main docs layout component

---
+++ src/routes/docs/components/__layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  import MiniFooter from "$components/bites/MiniFooter.svelte"
+  import PageNavigation from "$components/bites/PageNavigation.svelte"
+  import { pageNavigation } from "$lib/stores/site/Util";
+</script>
+
+<div class="uk-margin-large-top">
+  <div class="uk-grid-small" uk-grid>
+    <div class="uk-width-expand">
+      <slot></slot>
+    </div>
+    <div class="navigation-wrapper uk-width-1-4 uk-visible@m">
+      <PageNavigation navigation={$pageNavigation} />
+    </div>
+  </div>
+</div>
+
+<MiniFooter />
