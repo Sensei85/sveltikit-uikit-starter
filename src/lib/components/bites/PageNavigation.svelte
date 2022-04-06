acb368bcb0121ad2ff6cb9ef232efdc260e8175d
sensei85 <agbottahw@gmail.com>
2022-04-06 03:18:33 -0700

    [NEW] - New page navigation component. Just like the one on Uikit documentation page.

---
+++ src/lib/components/bites/PageNavigation.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { page } from "$app/stores";
+  export let navigation
+</script>
+
+<div class="uk-sticky uk-sticky-fixed uk-visible@m" uk-sticky="offset: 100; bottom: ~ .navigation-wrapper">
+  <div class="uk-width-auto">
+    <ul name="navigation" class="uk-nav uk-nav-default">
+      {#each navigation as nav}
+        <li class:uk-active={$page.url.href.includes(nav?.path)}>
+          <a href="{nav?.path}">{nav?.title}</a>
+        </li>
+      {/each}
+    </ul>
+  </div>
+</div>
