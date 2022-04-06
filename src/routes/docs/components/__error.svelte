c8d6e2dad85047b59440147631de2f501ca66699
sensei85 <agbottahw@gmail.com>
2022-04-06 03:22:01 -0700

    [NEW] - Error page created when trying to view a page in documentation dir

---
+++ src/routes/docs/components/__error.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+  <title>404 | Page Not Found</title>
+</svelte:head>
+
+<script context="module">
+  /** @type {import('@sveltejs/kit').ErrorLoad} */
+  export function load({ error, status }) {
+    return {
+      props: {
+        title: `${status}: ${error.message}`
+      }
+    }
+  }
+</script>
+
+<script>
+  export let title;
+</script>
+
+<h1>{title}</h1>
