485fc48cd35ee743cfe78b15c0844a71c4cd98ca
sensei85 <agbottahw@gmail.com>
2022-04-07 17:21:21 -0700

    [FIX] - Adjusted relative path to how uikit core js is loaded into project

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,6 @@
 <script>
 	import '../app.scss'
-	import './../../node_modules/uikit/dist/js/uikit-core'
-
+	import '../../node_modules/uikit/dist/js/uikit-core'
 	import Navbar from '$components/partials/Navbar.svelte'
 	import MobileNavbar from '$components/partials/MobileNavbar.svelte'
 	import MobileSideMenu from '$components/partials/MobileSideMenu.svelte'
