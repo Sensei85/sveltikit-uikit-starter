30a74c73d7b64839f18b3086e1efa85814e3edee
sensei85 <agbottahw@gmail.com>
2022-04-09 10:39:05 -0700

    [FIX] - Added fix for universal image container preview height in grid, text-truncate on file name and reduces figcaption padding.

--- src/lib/components/docs/FileItem.svelte
+++ src/lib/components/docs/FileItem.svelte
@@ -5,15 +5,18 @@
 
 <figure class="preview-item uk-flex uk-flex-column">
   <div class="image">
-    <img src="{result}" alt="" height="300" width="480">
+    <img src="{result}" title={name} alt="" height="300" width="480">
   </div>
-  <figcaption class="uk-padding-small">
-    <span class="uk-text-small uk-text-break">{name}</span>
+  <figcaption>
+    <span title={name} class="uk-text-small">{name}</span>
   </figcaption>
 </figure>
 
 <style lang="scss">
   figure {
+    .image {
+      height: 7.5rem;
+    }
     img {
       border-top-left-radius: .35rem;
       border-top-right-radius: .35rem;
@@ -24,8 +27,18 @@
       border-bottom-left-radius: .35rem;
       border-bottom-right-radius: .35rem;
       margin-top: .25rem;
-      background-color: #636e78;
+      background-color: teal;
       color: white;
+      padding: 0.35rem;
+      font-size: .15rem;
+      line-height: .15rem;
+      span {
+        display: inline-block;
+        width: 100px;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+      }
     }
   }
 </style>
